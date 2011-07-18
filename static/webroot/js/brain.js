b6af5de3774211599c3a910ea2f4922e1fd2fd05
Lawrence Wang <lawrence@levityisland.com>
2011-07-17 18:29:31 -0700

    connect input to milktruck

--- static/webroot/js/brain.js
+++ static/webroot/js/brain.js
@@ -1,13 +1,39 @@
 $(function() {
     
     window.Socket = io.connect('http://localhost:8081');
-    
-    new Hand({
-        id: 'left'
+
+    Socket.on('message', function(d) {
+        var data = eval('(' + d + ')');
+        var axis = data[0], val = data[1];
+        // console.log(axis);
+        
+        if (axis == "/x") {
+          if (val < 0.48) {
+            leftButtonDown = true;
+            rightButtonDown = false;
+          } else if (val > 0.52) {
+            leftButtonDown = false;
+            rightButtonDown = true;
+          } else {
+            leftButtonDown = false;
+            rightButtonDown = false;
+          }
+        } else if (axis == '/y') {
+          if (val < 0.48) {
+            gasButtonDown = false;
+            reverseButtonDown = true;
+          } else if (val > 0.52) {
+            gasButtonDown = true;
+            reverseButtonDown = false;
+          } else {
+            gasButtonDown = false;
+            reverseButtonDown = false;
+          }
+        }
+        
     });
     
-    new Hand({
-        id: 'right'
-    });
+    // new Hand({id: 'left'});
+    // new Hand({id: 'right'});
     
 });
